fefb01846d4d5cee63756838eb0ae058d02868b9
satoshi toyama <satoshi.toyama@route06.co.jp>
2024-02-27 09:58:04 +0900

    update

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,14 @@
 import { ActionFunctionArgs, json } from "@remix-run/cloudflare";
 import { Form, useActionData, useNavigation } from "@remix-run/react";
+import { CheckIcon } from "lucide-react";
 import { OpenAI } from "openai";
+import { useEffect, useState } from "react";
 import invariant from "tiny-invariant";
 import { Badge } from "~/components/ui/badge";
 import { Button } from "~/components/ui/button";
+import { Card, CardContent } from "~/components/ui/card";
 import { Label } from "~/components/ui/label";
+import { cn } from "~/lib/utils";
 
 export const action = async ({ request, context }: ActionFunctionArgs) => {
   const formData = await request.formData();
@@ -38,6 +42,13 @@ assistant: 赤色`,
 export default function Index() {
   const result = useActionData<typeof action>();
   const navigation = useNavigation();
+  const [objective1, setObjective1] = useState(false);
+  useEffect(() => {
+    if (result?.assistant == null) {
+      return;
+    }
+    setObjective1(true);
+  }, [result]);
   return (
     <div className="flex h-screen flex-col divide-y overflow-y-hidden">
       <header className="flex h-12 items-center px-8 font-bold">
@@ -52,14 +63,16 @@ export default function Index() {
                 読んで 書いて 使ってわかるプロンプトエンジニアリング
               </strong>
               」
-              へようこそ！このチュートリアルでは、プロンプトエンジニアリングの概念や、その実装方法について学ぶことができます。
+              へようこそ！このレッスンでは、プロンプトエンジニアリングの概念や、その実装方法について学ぶことができます。
             </p>
           </section>
           <section>
             <h2 className="py-4 text-xl">レッスン1: 空は？</h2>
             <p>
               まずは、シンプルなプロンプトから始めましょう。右の
-              <Badge className="mx-1">プロンプト</Badge>
+              <Label htmlFor="prompt">
+                <Badge className="mx-1">プロンプト</Badge>
+              </Label>
               と書かれているテキストエリアに
               <span className="bg-muted rounded px-2 py-0.5">空は</span>
               と入力して
@@ -72,7 +85,7 @@ export default function Index() {
         </div>
         <Form className="flex w-full flex-col divide-y" method="post">
           <div className="h-full">
-            <div className="h-full flex flex-col divide-y">
+            <div className="flex h-full flex-col divide-y">
               <div className="flex-1 p-8">
                 <Label htmlFor="prompt" className="mb-1 block">
                   <Badge>システムプロンプト</Badge>
@@ -81,7 +94,7 @@ export default function Index() {
                   id="system"
                   name="system"
                   placeholder="レッスン6に進むとここが使えるようになります"
-                  className="h-full w-full resize-none outline-none cursor-not-allowed"
+                  className="h-full w-full cursor-not-allowed resize-none outline-none"
                   readOnly
                 ></textarea>
               </div>
@@ -104,7 +117,7 @@ export default function Index() {
             </Button>
           </div>
         </Form>
-        <div className="w-[400px] p-8 shrink-0">
+        <div className="w-[400px] shrink-0 p-8">
           <div className="mb-1">
             <Badge>結果</Badge>
           </div>
@@ -117,6 +130,32 @@ export default function Index() {
           ) : (
             <p>{result.assistant}</p>
           )}
+          <Card className="mt-6">
+            <CardContent className="pt-4">
+              <p className="mb-2 font-bold">目標</p>
+              <div className="flex items-center">
+                <CheckIcon
+                  className={cn("mr-2 h-5 w-5", {
+                    "text-zinc-300": !objective1,
+                    "text-zinc-700": objective1,
+                  })}
+                />
+                <p>
+                  <Badge className="mr-1">結果</Badge>に青色と表示される
+                </p>
+              </div>
+              {objective1 && (
+                <div className="mt-2 rounded bg-zinc-100 p-4">
+                  <p>
+                    いいですね。このように、入力された文章の続きを生成するのが生成AIの基本的な使い方です。
+                  </p>
+                  <p>
+                    そして、生成内容を調整する技法がプロンプトエンジニアリングです。次のレッスンではもう少し抽象的な内容を生成してみましょう。
+                  </p>
+                </div>
+              )}
+            </CardContent>
+          </Card>
         </div>
       </main>
     </div>
